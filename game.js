fc646cb44d9b6f735923b92a1b8a5ecd26615744
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-27 10:28:02 -0600

    innerWidth and innerHeight

--- game.js
+++ game.js
@@ -4,11 +4,14 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', starGame);
 
 function starGame () {
-    game.fillRect(0,0,100,100); // Dibujar en el canvas
-    game.clearRect(0,0,0,0); // Limpiar o borrar en el canvas
+    canvas.setAttribute('width', window.innerWidth * 0.75);
+    canvas.setAttribute('height', window.innerHeight * 0.75);
 
-    game.font = '25px Verdana'; // Tamano y fuente de texto
-    game.fillStyle = 'purple'; // Indicar color de texto
-    game.textAlign = 'right'; // Alinear texto
-    game.fillText('Christopher', 150, 50); // Dibujar texto e indicar posicion
+    // game.fillRect(0,0,100,100); // Dibujar en el canvas
+    // game.clearRect(0,0,0,0); // Limpiar o borrar en el canvas
+
+    // game.font = '25px Verdana'; // Tamano y fuente de texto
+    // game.fillStyle = 'purple'; // Indicar color de texto
+    // game.textAlign = 'right'; // Alinear texto
+    // game.fillText('Christopher', 150, 50); // Dibujar texto e indicar posicion
 }
