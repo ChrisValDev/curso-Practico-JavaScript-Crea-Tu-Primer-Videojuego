b35a98f7f9fb4fc199979271d74d0eae88a17f95
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-27 11:22:13 -0600

    Size of the canvas and his elements

--- game.js
+++ game.js
@@ -4,8 +4,26 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', starGame);
 
 function starGame () {
-    canvas.setAttribute('width', window.innerWidth * 0.75);
-    canvas.setAttribute('height', window.innerHeight * 0.75);
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = canvasSize / 10;
+
+    console.log({canvasSize, elementsSize})
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);        
+    }
 
     // game.fillRect(0,0,100,100); // Dibujar en el canvas
     // game.clearRect(0,0,0,0); // Limpiar o borrar en el canvas
