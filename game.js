d751ecbf43e9dc2e21c478a42e14c533e81d6caa
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-27 10:17:56 -0600

    Canavas: fillRect, clearRect, fillText

--- game.js
+++ game.js
@@ -5,5 +5,10 @@ window.addEventListener('load', starGame);
 
 function starGame () {
     game.fillRect(0,0,100,100); // Dibujar en el canvas
-    game.clearRect(); // Limpiar o borrar en el canvas
+    game.clearRect(0,0,0,0); // Limpiar o borrar en el canvas
+
+    game.font = '25px Verdana'; // Tamano y fuente de texto
+    game.fillStyle = 'purple'; // Indicar color de texto
+    game.textAlign = 'right'; // Alinear texto
+    game.fillText('Christopher', 150, 50); // Dibujar texto e indicar posicion
 }
