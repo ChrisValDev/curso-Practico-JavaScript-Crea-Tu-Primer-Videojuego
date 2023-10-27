00a2bc7fddf7fa28f56e5a9cc834f31dd1093192
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-27 11:39:04 -0600

    Canvas responsive

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+let canvasSize;
+let elementsSize;
 
-window.addEventListener('load', starGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-function starGame () {
-    let canvasSize;
-
+function setCanvasSize () {
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
     } else {
@@ -15,21 +16,17 @@ function starGame () {
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    const elementsSize = canvasSize / 10;
+    elementsSize = canvasSize / 10;
 
-    console.log({canvasSize, elementsSize})
+    starGame()
+}
 
+function starGame () {
+    
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
+
     for (let i = 1; i <= 10; i++) {
         game.fillText(emojis['X'], elementsSize * i, elementsSize);        
     }
-
-    // game.fillRect(0,0,100,100); // Dibujar en el canvas
-    // game.clearRect(0,0,0,0); // Limpiar o borrar en el canvas
-
-    // game.font = '25px Verdana'; // Tamano y fuente de texto
-    // game.fillStyle = 'purple'; // Indicar color de texto
-    // game.textAlign = 'right'; // Alinear texto
-    // game.fillText('Christopher', 150, 50); // Dibujar texto e indicar posicion
 }
